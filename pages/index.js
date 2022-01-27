303485def52e6c76d3cf3adef4180f16eeb66f6a
SHAHULDEV22 <90827098+shahul2218@users.noreply.github.com>
2022-01-27 15:32:53 +0530

    first commit

--- pages/index.js
+++
@@ -1,6 +0,0 @@
-import Head from "next/head";
-import Image from "next/image";
-
-export default function Home() {
-  return <h1 className="text-3xl font-bold underline">Home</h1>;
-}
---
+++ pages/index.jsx
@@ -0,0 +1,13 @@
+// import Head from "next/head";
+// import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+      <h1>hello</h1>
+      <h1>hello</h1>
+      <h1>hello</h1>
+      <h1>hello</h1>
+    </>
+  );
+}
