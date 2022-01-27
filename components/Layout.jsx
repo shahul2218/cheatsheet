7e3bd223e55a04b50078b3c4b29390de5834d8b3
SHAHULDEV22 <90827098+shahul2218@users.noreply.github.com>
2022-01-27 16:48:14 +0530

    first commit

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -5,7 +5,9 @@ export default function Layout({ children }) {
   return (
     <>
       <Navbar />
-      <main>{children}</main>
+      <main className="md:w-9/12 sm:w-10/12 w-full px-3 sm:px-0 mx-auto">
+        {children}
+      </main>
       <Footer />
     </>
   );
