4a5df217bccf18b0323b36940a5d19ef5f3438e1
SHAHULDEV22 <90827098+shahul2218@users.noreply.github.com>
2022-01-28 15:12:31 +0530

    first commit

--- components/MainPage.jsx
+++ components/MainPage.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import Image from "next/image";
 
 const MainPage = () => {
   return (
@@ -13,12 +13,207 @@ const MainPage = () => {
           pug.
         </p>
       </div>
-      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4">
-        <div>1</div>
-        <div>2</div>
-        <div>3</div>
-        <div>4</div>
-        <div>5</div>
+      <div className="grid grid-cols-3 sm:grid-cols-4 md:grid-cols-5 lg:grid-cols-7 xl:grid-cols-9 gap-4">
+        <div className="morphic-card-flat mx-auto">
+          1
+          <Image
+            src="/html.png"
+            alt="site brand logo"
+            width={125}
+            height={125}
+            className="w-full mx-auto"
+          />
+        </div>
+        <div className="morphic-card-flat mx-auto">
+          2
+          <Image
+            src="/html.png"
+            alt="site brand logo"
+            width={125}
+            height={125}
+            className="w-full mx-auto"
+          />
+        </div>
+        <div className="morphic-card-flat mx-auto">
+          3
+          <Image
+            src="/html.png"
+            alt="site brand logo"
+            width={125}
+            height={125}
+            className="w-full mx-auto"
+          />
+        </div>
+        <div className="morphic-card-flat mx-auto">
+          4
+          <Image
+            src="/html.png"
+            alt="site brand logo"
+            width={125}
+            height={125}
+            className="w-full mx-auto"
+          />
+        </div>
+        <div className="morphic-card-flat mx-auto">
+          5
+          <Image
+            src="/html.png"
+            alt="site brand logo"
+            width={125}
+            height={125}
+            className="w-full mx-auto"
+          />
+        </div>
+        <div className="morphic-card-flat mx-auto">
+          6
+          <Image
+            src="/html.png"
+            alt="site brand logo"
+            width={125}
+            height={125}
+            className="w-full mx-auto"
+          />
+        </div>
+        <div className="morphic-card-flat mx-auto">
+          7
+          <Image
+            src="/html.png"
+            alt="site brand logo"
+            width={125}
+            height={125}
+            className="w-full mx-auto"
+          />
+        </div>
+        <div className="morphic-card-flat mx-auto">
+          8
+          <Image
+            src="/html.png"
+            alt="site brand logo"
+            width={125}
+            height={125}
+            className="w-full mx-auto"
+          />
+        </div>
+        <div className="morphic-card-flat mx-auto">
+          9
+          <Image
+            src="/html.png"
+            alt="site brand logo"
+            width={125}
+            height={125}
+            className="w-full mx-auto"
+          />
+        </div>
+        <div className="morphic-card-flat mx-auto">
+          10
+          <Image
+            src="/html.png"
+            alt="site brand logo"
+            width={125}
+            height={125}
+            className="w-full mx-auto"
+          />
+        </div>
+        <div className="morphic-card-flat mx-auto">
+          11
+          <Image
+            src="/html.png"
+            alt="site brand logo"
+            width={125}
+            height={125}
+            className="w-full mx-auto"
+          />
+        </div>
+        <div className="morphic-card-flat mx-auto">
+          12
+          <Image
+            src="/html.png"
+            alt="site brand logo"
+            width={125}
+            height={125}
+            className="w-full mx-auto"
+          />
+        </div>
+        <div className="morphic-card-flat mx-auto">
+          13
+          <Image
+            src="/html.png"
+            alt="site brand logo"
+            width={125}
+            height={125}
+            className="w-full mx-auto"
+          />
+        </div>
+        <div className="morphic-card-flat mx-auto">
+          14
+          <Image
+            src="/html.png"
+            alt="site brand logo"
+            width={125}
+            height={125}
+            className="w-full mx-auto"
+          />
+        </div>
+        <div className="morphic-card-flat mx-auto">
+          15
+          <Image
+            src="/html.png"
+            alt="site brand logo"
+            width={125}
+            height={125}
+            className="w-full mx-auto"
+          />
+        </div>
+        <div className="morphic-card-flat mx-auto">
+          16
+          <Image
+            src="/html.png"
+            alt="site brand logo"
+            width={125}
+            height={125}
+            className="w-full mx-auto"
+          />
+        </div>
+        <div className="morphic-card-flat mx-auto">
+          17
+          <Image
+            src="/html.png"
+            alt="site brand logo"
+            width={125}
+            height={125}
+            className="w-full mx-auto"
+          />
+        </div>
+        <div className="morphic-card-flat mx-auto">
+          18
+          <Image
+            src="/html.png"
+            alt="site brand logo"
+            width={125}
+            height={125}
+            className="w-full mx-auto"
+          />
+        </div>
+        <div className="morphic-card-flat mx-auto">
+          19
+          <Image
+            src="/html.png"
+            alt="site brand logo"
+            width={125}
+            height={125}
+            className="w-full mx-auto"
+          />
+        </div>
+        <div className="morphic-card-flat mx-auto">
+          20
+          <Image
+            src="/html.png"
+            alt="site brand logo"
+            width={125}
+            height={125}
+            className="w-full mx-auto"
+          />
+        </div>
       </div>
     </section>
   );
