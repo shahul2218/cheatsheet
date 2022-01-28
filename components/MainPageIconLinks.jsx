fdf8362032080d7ad402c69b6d319154b938863c
SHAHULDEV22 <90827098+shahul2218@users.noreply.github.com>
2022-01-28 15:50:28 +0530

    first commit

---
+++ components/MainPageIconLinks.jsx
@@ -0,0 +1,215 @@
+import Image from "next/image";
+
+const MainPageIconLinks = () => {
+  return (
+    <>
+      <h2 className="py-6 text-red-500 font-bold tracking-wider">
+        <span className="px-4 py-2 outer-morphic-btn-after">Starts With A</span>
+      </h2>
+      <div className="grid grid-cols-3 sm:grid-cols-4 md:grid-cols-5 lg:grid-cols-7 xl:grid-cols-9 gap-4">
+        <div className="morphic-card-flat mx-auto">
+          1
+          <Image
+            src="/html.png"
+            alt="site brand logo"
+            width={125}
+            height={125}
+            className="w-full mx-auto"
+          />
+        </div>
+        <div className="morphic-card-flat mx-auto">
+          2
+          <Image
+            src="/html.png"
+            alt="site brand logo"
+            width={125}
+            height={125}
+            className="w-full mx-auto"
+          />
+        </div>
+        <div className="morphic-card-flat mx-auto">
+          3
+          <Image
+            src="/html.png"
+            alt="site brand logo"
+            width={125}
+            height={125}
+            className="w-full mx-auto"
+          />
+        </div>
+        <div className="morphic-card-flat mx-auto">
+          4
+          <Image
+            src="/html.png"
+            alt="site brand logo"
+            width={125}
+            height={125}
+            className="w-full mx-auto"
+          />
+        </div>
+        <div className="morphic-card-flat mx-auto">
+          5
+          <Image
+            src="/html.png"
+            alt="site brand logo"
+            width={125}
+            height={125}
+            className="w-full mx-auto"
+          />
+        </div>
+        <div className="morphic-card-flat mx-auto">
+          6
+          <Image
+            src="/html.png"
+            alt="site brand logo"
+            width={125}
+            height={125}
+            className="w-full mx-auto"
+          />
+        </div>
+        <div className="morphic-card-flat mx-auto">
+          7
+          <Image
+            src="/html.png"
+            alt="site brand logo"
+            width={125}
+            height={125}
+            className="w-full mx-auto"
+          />
+        </div>
+        <div className="morphic-card-flat mx-auto">
+          8
+          <Image
+            src="/html.png"
+            alt="site brand logo"
+            width={125}
+            height={125}
+            className="w-full mx-auto"
+          />
+        </div>
+        <div className="morphic-card-flat mx-auto">
+          9
+          <Image
+            src="/html.png"
+            alt="site brand logo"
+            width={125}
+            height={125}
+            className="w-full mx-auto"
+          />
+        </div>
+        <div className="morphic-card-flat mx-auto">
+          10
+          <Image
+            src="/html.png"
+            alt="site brand logo"
+            width={125}
+            height={125}
+            className="w-full mx-auto"
+          />
+        </div>
+        <div className="morphic-card-flat mx-auto">
+          11
+          <Image
+            src="/html.png"
+            alt="site brand logo"
+            width={125}
+            height={125}
+            className="w-full mx-auto"
+          />
+        </div>
+        <div className="morphic-card-flat mx-auto">
+          12
+          <Image
+            src="/html.png"
+            alt="site brand logo"
+            width={125}
+            height={125}
+            className="w-full mx-auto"
+          />
+        </div>
+        <div className="morphic-card-flat mx-auto">
+          13
+          <Image
+            src="/html.png"
+            alt="site brand logo"
+            width={125}
+            height={125}
+            className="w-full mx-auto"
+          />
+        </div>
+        <div className="morphic-card-flat mx-auto">
+          14
+          <Image
+            src="/html.png"
+            alt="site brand logo"
+            width={125}
+            height={125}
+            className="w-full mx-auto"
+          />
+        </div>
+        <div className="morphic-card-flat mx-auto">
+          15
+          <Image
+            src="/html.png"
+            alt="site brand logo"
+            width={125}
+            height={125}
+            className="w-full mx-auto"
+          />
+        </div>
+        <div className="morphic-card-flat mx-auto">
+          16
+          <Image
+            src="/html.png"
+            alt="site brand logo"
+            width={125}
+            height={125}
+            className="w-full mx-auto"
+          />
+        </div>
+        <div className="morphic-card-flat mx-auto">
+          17
+          <Image
+            src="/html.png"
+            alt="site brand logo"
+            width={125}
+            height={125}
+            className="w-full mx-auto"
+          />
+        </div>
+        <div className="morphic-card-flat mx-auto">
+          18
+          <Image
+            src="/html.png"
+            alt="site brand logo"
+            width={125}
+            height={125}
+            className="w-full mx-auto"
+          />
+        </div>
+        <div className="morphic-card-flat mx-auto">
+          19
+          <Image
+            src="/html.png"
+            alt="site brand logo"
+            width={125}
+            height={125}
+            className="w-full mx-auto"
+          />
+        </div>
+        <div className="morphic-card-flat mx-auto">
+          20
+          <Image
+            src="/html.png"
+            alt="site brand logo"
+            width={125}
+            height={125}
+            className="w-full mx-auto"
+          />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default MainPageIconLinks;
